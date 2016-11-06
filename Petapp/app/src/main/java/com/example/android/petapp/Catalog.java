a529582178181d06bbc591237102c1b2aa9135be
ShehryaAhmed <Shehryar.ahmed@nhu.edu.pk>
2016-11-06 20:40:06 +0500

    set listview

--- Petapp/app/src/main/java/com/example/android/petapp/Catalog.java
+++ Petapp/app/src/main/java/com/example/android/petapp/Catalog.java
@@ -25,12 +25,14 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import java.util.ArrayList;
 
 public class Catalog extends AppCompatActivity {
-
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
     private PetDBhelper mDbhelper = new PetDBhelper(this);
+
+
+   private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,9 +49,7 @@ public class Catalog extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-
-        ListView listView = (ListView) findViewById(R.id.list);
+        listView = (ListView) findViewById(R.id.list);
 
         PetAdapter petAdapter = new PetAdapter(this, displayDatabaseInfo());
 
@@ -57,85 +57,6 @@ public class Catalog extends AppCompatActivity {
 
     }
 
-        private ArrayList<Pets> displayDatabaseInfo () {
-            // Create and/or open a database to read from it
-            SQLiteDatabase db = mDbhelper.getReadableDatabase();
-
-            ArrayList<Pets> arrayList = new ArrayList<>();
-
-            // Define a projection that specifies which columns from the database
-            // you will actually use after this query.
-            String[] projection = {
-                    PetContract.PetEntry._ID,
-                    PetContract.PetEntry.COLUMN_PET_NAME,
-                    PetContract.PetEntry.COLUMN_PET_BREED,
-                    PetContract.PetEntry.COLUMN_PET_WEIGHT,
-                    PetContract.PetEntry.COLUMN_PET_GENDER,
-            };
-
-            // Perform a query on the pets table
-            Cursor cursor = db.query(
-                    PetContract.PetEntry.TABLE_NAME,   // The table to query
-                    projection,            // The columns to return
-                    null,                  // The columns for the WHERE clause
-                    null,                  // The values for the WHERE clause
-                    null,                  // Don't group the rows
-                    null,                  // Don't filter by row groups
-                    null);                   // The sort order
-
-           // TextView displayView = (TextView) findViewById(R.id.text_view_pet);
-
-            try {
-                // Create a header in the Text View that looks like this:
-                //
-                // The pets table contains <number of rows in Cursor> pets.
-                // _id - name - breed - gender - weight
-                //
-                // In the while loop below, iterate through the rows of the cursor and display
-                // the information from each column in this order.
-                /*
-                displayView.setText("The pets table contains " + cursor.getCount() + " pets.\n\n");
-                displayView.append(PetContract.PetEntry._ID + " - " +
-                        PetContract.PetEntry.COLUMN_PET_NAME + " - " +
-                        PetContract.PetEntry.COLUMN_PET_BREED + " - " +
-                        PetContract.PetEntry.COLUMN_PET_GENDER + " - " +
-                        PetContract.PetEntry.COLUMN_PET_WEIGHT + "\n");
-*/
-                // Figure out the index of each column
-                int idColumnIndex = cursor.getColumnIndex(PetContract.PetEntry._ID);
-                int nameColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_NAME);
-                int breedColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_BREED);
-                int genderColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_GENDER);
-                int weightColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_WEIGHT);
-
-                // Iterate through all the returned rows in the cursor
-                while (cursor.moveToNext()) {
-                    // Use that index to extract the String or Int value of the word
-                    // at the current row the cursor is on.
-                    int currentID = cursor.getInt(idColumnIndex);
-                    String currentName = cursor.getString(nameColumnIndex);
-                    String currentBreed = cursor.getString(breedColumnIndex);
-                    int currentGender = cursor.getInt(genderColumnIndex);
-                    int currentWeight = cursor.getInt(weightColumnIndex);
-                    // Display the values from each column of the current row in the cursor in the TextView
-              /*      displayView.append(("\n" + currentID + " - " +
-                            currentName + " - " +
-                            currentBreed + " - " +
-                            currentGender + " - " +
-                            currentWeight));*/
-
-
-                    arrayList.add(new Pets(currentName,currentBreed,currentGender,currentWeight));
-
-                }
-
-            } finally {
-                // Always close the cursor when you're done reading from it. This releases all its
-                // resources and makes it invalid.
-                cursor.close();
-            }
-        return arrayList;
-        }
 
 
 
@@ -163,8 +84,9 @@ public class Catalog extends AppCompatActivity {
     public void onStart() {
         super.onStart();
 
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        PetAdapter petAdapter = new PetAdapter(this, displayDatabaseInfo());
+
+        listView.setAdapter(petAdapter);
 
     }
 
@@ -175,4 +97,84 @@ public class Catalog extends AppCompatActivity {
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
     }
-}
+
+    private ArrayList<Pets> displayDatabaseInfo () {
+        // Create and/or open a database to read from it
+        SQLiteDatabase db = mDbhelper.getReadableDatabase();
+
+        ArrayList<Pets> arrayList = new ArrayList<>();
+
+        // Define a projection that specifies which columns from the database
+        // you will actually use after this query.
+        String[] projection = {
+                PetContract.PetEntry._ID,
+                PetContract.PetEntry.COLUMN_PET_NAME,
+                PetContract.PetEntry.COLUMN_PET_BREED,
+                PetContract.PetEntry.COLUMN_PET_WEIGHT,
+                PetContract.PetEntry.COLUMN_PET_GENDER,
+        };
+
+        // Perform a query on the pets table
+        Cursor cursor = db.query(
+                PetContract.PetEntry.TABLE_NAME,   // The table to query
+                projection,            // The columns to return
+                null,                  // The columns for the WHERE clause
+                null,                  // The values for the WHERE clause
+                null,                  // Don't group the rows
+                null,                  // Don't filter by row groups
+                null);                   // The sort order
+
+        // TextView displayView = (TextView) findViewById(R.id.text_view_pet);
+
+        try {
+            // Create a header in the Text View that looks like this:
+            //
+            // The pets table contains <number of rows in Cursor> pets.
+            // _id - name - breed - gender - weight
+            //
+            // In the while loop below, iterate through the rows of the cursor and display
+            // the information from each column in this order.
+                /*
+                displayView.setText("The pets table contains " + cursor.getCount() + " pets.\n\n");
+                displayView.append(PetContract.PetEntry._ID + " - " +
+                        PetContract.PetEntry.COLUMN_PET_NAME + " - " +
+                        PetContract.PetEntry.COLUMN_PET_BREED + " - " +
+                        PetContract.PetEntry.COLUMN_PET_GENDER + " - " +
+                        PetContract.PetEntry.COLUMN_PET_WEIGHT + "\n");
+*/
+            // Figure out the index of each column
+            int idColumnIndex = cursor.getColumnIndex(PetContract.PetEntry._ID);
+            int nameColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_NAME);
+            int breedColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_BREED);
+            int genderColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_GENDER);
+            int weightColumnIndex = cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_WEIGHT);
+
+            // Iterate through all the returned rows in the cursor
+            while (cursor.moveToNext()) {
+                // Use that index to extract the String or Int value of the word
+                // at the current row the cursor is on.
+                int currentID = cursor.getInt(idColumnIndex);
+                String currentName = cursor.getString(nameColumnIndex);
+                String currentBreed = cursor.getString(breedColumnIndex);
+                int currentGender = cursor.getInt(genderColumnIndex);
+                int currentWeight = cursor.getInt(weightColumnIndex);
+                // Display the values from each column of the current row in the cursor in the TextView
+              /*      displayView.append(("\n" + currentID + " - " +
+                            currentName + " - " +
+                            currentBreed + " - " +
+                            currentGender + " - " +
+                            currentWeight));*/
+
+
+                arrayList.add(new Pets(currentName, currentBreed, currentGender, currentWeight));
+
+            }
+
+        } finally {
+            // Always close the cursor when you're done reading from it. This releases all its
+            // resources and makes it invalid.
+            cursor.close();
+        }
+        return arrayList;
+
+    }}
